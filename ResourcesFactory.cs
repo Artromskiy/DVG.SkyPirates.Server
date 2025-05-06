2647deb32eba6d9eb5c52bd1a1ace3076d3b892e
Artromskiy <artromskiy@gmail.com>
2025-05-06 22:51:20 +0300

    Resources factory

---
+++ ResourcesFactory.cs
@@ -0,0 +1,15 @@
+﻿using DVG.Core;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace DVG.SkyPirates.Server
+{
+    internal class ResourcesFactory<T> : IPathFactory<T>
+    {
+        public T Create(string parameters)
+        {
+            var text = File.ReadAllText(parameters);
+            return JsonConvert.DeserializeObject<T>(text);
+        }
+    }
+}
